1e25b7d14c072b842d1388d77de24ee30d4d8c24
Lars Kiesow <lkiesow@uos.de>
2012-11-21 03:49:58 +0100

    Add user table

--- create_db.py
+++ create_db.py
@@ -13,6 +13,8 @@ cursor.execute('''CREATE TABLE documents
 					language text, relation text, coverage text, rights text,
 					tags text, filename text ) ''')
 
-# cursor.execute('''insert into documents (title, creator, subject, description, publisher, contributor, date, type, format, identifier, source, language, relation, coverage, rights, tags, filename) values ( 'XMP Specification - Part 1, Data model, Serialization, and Core Properties', 'Adobe Systems', 'metadata. XMP, specification', 'Covers the basic metadata representation model that is the foundation of the XMP standard format. The Data Model prescribes how XMP metadata can be organized; it is independent of file format or specific usage. The Serialization Model prescribes how the Data Model is represented in XML, specifically RDF.', "Adobe Systems", '', '2012-04', 'medium', 'application/pdf', 'XMPSpec1', 'http://www.adobe.com/devnet/xmp.html', 'en', '', '', '', 'xmp, specs, specification, metadata', 'XMPSpecificationPart1.pdf' )''' )
+cursor.execute('''CREATE TABLE user
+					( username text primary key, 
+					passwd text, salt text )''')
 
 conn.commit()
